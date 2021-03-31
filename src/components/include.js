e113a9a9c892314b99066e74f187c8149231a8cb
poeticAndroid <i@poeticAndroid.online>
2021-03-31 18:36:25 +0200

    smarter include

--- src/components/include.js
+++ src/components/include.js
@@ -1,19 +1,17 @@
 /* global AFRAME, THREE */
 
-let loading = false
-
 AFRAME.registerComponent("include", {
   schema: { type: "string" },
 
   update: async function () {
-    if (this.data && !loading) {
-      loading = true
-      console.log("Including", this.data)
-      this.el.outerHTML = await (await fetch(this.data)).text()
-      loading = false
+    if (this.data && !this.el.sceneEl._including_) {
+      this.el.sceneEl._including_ = true
+      let response = await fetch(this.data)
+      if (response.status >= 200 && response.status < 300) this.el.outerHTML = await (response).text()
+      else this.el.removeAttribute("include")
+      this.el.sceneEl._including_ = false
       let next = this.el.sceneEl.querySelector("[include]")
-      if (next)
-        next.components.include.update()
+      if (next) next.components.include.update()
     }
   }
 })
