8559efbdf6586fc0ce63a56794876cb2ee8648a8
poeticAndroid <i@poeticAndroid.online>
2021-04-09 16:51:49 +0200

    smarter include

--- src/components/include.js
+++ src/components/include.js
@@ -6,12 +6,30 @@ AFRAME.registerComponent("include", {
   init: async function () {
     if (this.data && !this.el.sceneEl._including_) {
       this.el.sceneEl._including_ = true
-      let response = await fetch(this.data)
-      if (response.status >= 200 && response.status < 300) this.el.outerHTML = await (response).text()
+      let baseUrl = ""
+      let baseEl = this.el
+      while (baseEl && !baseEl.dataset["includeBase"]) baseEl = baseEl.parentElement
+      if (baseEl) baseUrl = baseEl.dataset["includeBase"]
+      let url = this.data
+      if (url.substr(0, 1) !== "/" && url.substr(0, 4) !== "http") url = baseUrl + url
+      if (url.indexOf("/") >= 0) this.el.dataset["includeBase"] = url.substr(0, url.lastIndexOf("/") + 1)
+
+      let b4Content = this.el.outerHTML
+
+      let p1 = b4Content.indexOf(" ")
+      let p2 = b4Content.indexOf(" include=")
+      let attrs = b4Content.substr(p1, p2 - p1)
+
+      p1 = b4Content.indexOf("\"", p2 + 10) + 1
+      p2 = b4Content.indexOf(">")
+      attrs += b4Content.substr(p1, p2 - p1)
+
+      let response = await fetch(url)
+      if (response.status >= 200 && response.status < 300) this.el.outerHTML = await (await (response).text()).replace(">", " " + attrs + ">")
       else this.el.removeAttribute("include")
       this.el.sceneEl._including_ = false
       let next = this.el.sceneEl.querySelector("[include]")
-      if (next) next.components.include.init()
+      if (next && next.components && next.components.include) next.components.include.init()
     }
   }
 })
