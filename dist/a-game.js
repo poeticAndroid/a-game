5377a159de18b4ee474ab336fcdbc333f571df37
poeticAndroid <i@poeticAndroid.online>
2021-11-26 12:00:12 +0100

    documenting how `a-player` gets auto-populated

--- dist/a-game.js
+++ dist/a-game.js
@@ -2,7 +2,7 @@
 module.exports={
   "name": "a-game",
   "title": "A-Game",
-  "version": "0.38.8",
+  "version": "0.38.9",
   "description": "game components for A-Frame",
   "homepage": "https://github.com/poeticAndroid/a-game/blob/master/README.md",
   "main": "index.js",
